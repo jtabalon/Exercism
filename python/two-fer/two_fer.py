c3c4d725b3f7bf44a4fd6080e1923e4692fc5ebe
Joseph Tabalon <joseph.tabalon.jr@gmail.com>
2021-07-18 17:49:14 -1000

    change default to 'you' for more efficient code

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -1,8 +1,5 @@
-def two_fer(name=""):
-    if name=="":
-        return "One for you, one for me."
-    else:
-        return f"One for {name}, one for me."
+def two_fer(name="you"):
+	return f"One for {name}, one for me."
 
 if __name__=="__main__":
     two_fer()
