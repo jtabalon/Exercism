9b9d64a121a1a8fefff35937f60dfe367a0a9bb7
Joseph Tabalon <joseph.tabalon.jr@gmail.com>
2021-07-28 06:11:28 -1000

    add exception handling - empty list

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,9 +1,15 @@
 def latest(scores):
+    if not scores:
+        raise ValueError("Expected non-empty sequence")
     return scores[-1]
 
 def personal_best(scores):
+    if not scores:
+        raise ValueError("Expected non-empty sequence")
     return max(scores)
 
 def personal_top_three(scores):
+    if not scores:
+        raise ValueError("Expected non-empty sequence")
     scores = sorted(scores, reverse=True)
     return scores[:3]
