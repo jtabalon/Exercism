09b4a7ae0a4c9bf489ac0fc844b983a7721c99ec
Joseph Tabalon <joseph.tabalon.jr@gmail.com>
2021-07-28 06:09:28 -1000

    remove if statements

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,12 +1,9 @@
 def latest(scores):
-    if scores:
-        return scores[-1]
+    return scores[-1]
 
 def personal_best(scores):
-    if scores:
-        return max(scores)
+    return max(scores)
 
 def personal_top_three(scores):
     scores = sorted(scores, reverse=True)
-    if scores:
-        return scores[0:3]
+    return scores[:3]
