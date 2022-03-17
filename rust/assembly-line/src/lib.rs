1f875d187fd100eee1a4c2099d8bd66a961fb003
jtabalon <joseph@tabalon.io>
2022-03-16 20:05:46 -0700

    complete first iteration of production_rate_per_hour

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -2,10 +2,35 @@
 // to enable stricter warnings.
 #![allow(unused)]
 
+const RATE_ONE: f64 = 1.0;
+const RATE_TWO: f64 = 0.9;
+const RATE_THREE: f64 = 0.77;
+
+
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {}", speed)
+    // unimplemented!("calculate hourly production rate at speed: {}", speed)
+
+    let prod_rate = 
+        if speed < 1 && speed > 10 {
+            break;
+        }
+        else if speed <= 5 {
+            4.0 * speed
+        }
+        else if speed >= 5 && speed <= 8 {
+            4.0 * speed * 0.90
+        }
+        else if speed >= 9 ** speed <= 10 {
+            4.0 * speed * 0.77
+        };
+    
+    return prod_rate;
+
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    if speed < 1 && speed > 10 {
+        break;
+    }
+    // unimplemented!("calculate the amount of working items at speed: {}", speed)
 }
