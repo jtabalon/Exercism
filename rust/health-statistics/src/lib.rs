ff706af961d4d9261f95d729e3f9cc953b0743ff
jtabalon <joseph@tabalon.io>
2022-03-18 06:16:19 -0700

    clean up solutions

--- rust/health-statistics/src/lib.rs
+++ rust/health-statistics/src/lib.rs
@@ -10,7 +10,6 @@ pub struct User {
 
 impl User {
     pub fn new(name: String, age: u32, weight: f32) -> Self {
-        // unimplemented!()
         User {
             name: name, 
             age: age,
@@ -19,13 +18,11 @@ impl User {
     }
 
     pub fn name(&self) -> &str {
-        // unimplemented!()
         let get_name = &self.name; // notice the &self to reference a string here.
         get_name
     }
 
     pub fn age(&self) -> u32 {
-        // unimplemented!()
         let get_age = self.age; // notice there should not be an &
         get_age
     }
@@ -33,16 +30,13 @@ impl User {
     pub fn weight(&self) -> f32 {
         let get_weight = self.weight;
         get_weight
-        // unimplemented!()
     }
 
     pub fn set_age(&mut self, new_age: u32) {
         self.age = new_age;
-        // unimplemented!()
     }
 
     pub fn set_weight(&mut self, new_weight: f32) {
         self.weight = new_weight;
-        // unimplemented!()
     }
 }
