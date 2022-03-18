564c5a6b5f8c73787bddd5fcee2c8e9be386a481
jtabalon <joseph@tabalon.io>
2022-03-18 05:44:52 -0700

    clean up code

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -6,12 +6,7 @@ const RATE_ONE: f64 = 1.0;
 const RATE_TWO: f64 = 0.9;
 const RATE_THREE: f64 = 0.77;
 
-
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    // unimplemented!("calculate hourly production rate at speed: {}", speed)
-
-    // TODO: troubleshoot types + functions
-
     let prod_rate: f64 = 
         if speed < 5 {
             221.0 * (speed as f64)
@@ -22,15 +17,10 @@ pub fn production_rate_per_hour(speed: u8) -> f64 {
         else {
             221.0 * (speed as f64) * 0.77
         };
-    
     return prod_rate;
-
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-
     let items_per_minute: u32 = production_rate_per_hour(speed) as u32 / 60;
     return items_per_minute;
-    
-    // unimplemented!("calculate the amount of working items at speed: {}", speed)
 }
