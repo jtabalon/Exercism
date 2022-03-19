d315392c922c995c8ff32ff980a509b8bfca10ac
jtabalon <joseph@tabalon.io>
2022-03-19 16:46:22 -0700

    begin rust exercise - clock

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -2,14 +2,14 @@ pub struct Clock;
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+        // unimplemented!(
+        //     "Construct a new Clock from {} hours and {} minutes",
+        //     hours,
+        //     minutes
+        // );
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+        // unimplemented!("Add {} minutes to existing Clock time", minutes);
     }
 }
