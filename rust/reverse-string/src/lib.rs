c6ab7b07a2a9a3addb76beba1eb524fd0cb2d768
jtabalon <joseph@tabalon.io>
2022-05-27 19:42:54 -0700

    fix reverse string

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,15 +1,15 @@
-extern crate unicode-segmentation;
-use unicode-segmentation::UnicodeSegmentation;
+extern crate unicode_segmentation;
+use unicode_segmentation::UnicodeSegmentation;
 
 pub fn reverse(input: &str) -> String {
-    let drow: String = word
+    let drow: String = input
         .graphemes(true)
         .rev()
         .collect();
     
     drow
     
-    println!("{}", drow);
+    // println!("{}", drow);
 
 
 }
