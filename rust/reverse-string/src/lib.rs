3ccac44089d748590d77865248b19d48f8cb540d
jtabalon <joseph@tabalon.io>
2022-05-27 19:29:52 -0700

    initial implementation of reverse-string

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -2,6 +2,14 @@ extern crate unicode-segmentation;
 use unicode-segmentation::UnicodeSegmentation;
 
 pub fn reverse(input: &str) -> String {
+    let drow: String = word
+        .graphemes(true)
+        .rev()
+        .collect();
+    
+    drow
+    
+    println!("{}", drow);
 
 
 }
