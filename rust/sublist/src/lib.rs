3af1505ba1bcb5f3006fe4f239f597b3c0d42fc5
jtabalon <joseph@tabalon.io>
2022-05-29 09:58:32 -0700

    initial implementation of sublist

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -7,5 +7,20 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
+    let superlist = _second_list.is_empty()
+        || first_list
+            .windows(_second_list.len())
+            .any(|x| x == _second_list);
     
+    let sublist = _first_list.is_empty()
+        || second_list
+            .windows(_first_list.len())
+            .any(|x| x == _first_list);
+    
+    match (superlist, sublist) {
+        (true, true) => Comparison::Equal,
+        (true, false) => Comparison::Superlist,
+        (false, true) => Comparison::Sublist,
+        (false, false) => Comparison::Unequal
+    }
 }
